8ab94e7003c94c76fc91b00ab41329cca98084af
Felipe Beskow <felipebeskow@outlook.com>
2021-08-08 02:33:23 -0300

    WebSocket

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # webSocket
 Estudo de websocket
+
+
+Feito com base no vídeo https://www.youtube.com/watch?v=-jXfKDYJJvo
--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-module.exports = app;
+let messages = [];
+
+io.on('connection', socket=>{
+    console.log(`Socket conectado: ${socket.id}`);
+
+    socket.emit('previousMessages', messages);
+
+    socket.on('sendMessage', data=>{
+        messages.push(data);
+        socket.broadcast.emit('receivedMessage', data);
+    });
+});
+
+server.listen(3000);
