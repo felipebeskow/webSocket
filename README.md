7bae9c487736fac96d5fbcdc9d97e1c0754da91d
Felipe Beskow <felipebeskow@outlook.com>
2021-08-09 00:29:13 -0300

    readme e comentários no código

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-# webSocket
-Estudo de websocket
+# WebSockets: aplicação em tempo real
+## Objetivos:
+1. Conceito básico
+    1. Para que serve?
+    1. Quais problemas que podem ser resolvidos?
+    1. Cases: quem está utilizando?
+1. Momento prático
+    1. Uso da tecnologia na prática
+    1. Preparar um pequeno exemplo
 
+---
+[Slides da apresentação]()
 
-Feito com base no vídeo https://www.youtube.com/watch?v=-jXfKDYJJvo
+----
+
+## Biblioteca utilizadas:
+### Front-End
+* [Jquery - https://cdnjs.com/libraries/jquery](https://cdnjs.com/libraries/jquery)
+* [Socket.io - https://cdnjs.com/libraries/socket.io](https://cdnjs.com/libraries/socket.io)
+### Back-End
+* Express ~4.16.1
+* Socket.io ^4.1.3
--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 var express = require('express');
 var path = require('path');
 
-var indexRouter = require('./routes/index');
-
+//Criar sever http e socket
 var app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+//permitir o acesso ao front-end
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use('/', indexRouter);
+//definido rota http para o front
+app.get('/', function(req, res, next) {
+    res.render('index.html');
+  });
 
 let messages = [];
 
+//abre conexão entre cliente e servidor
 io.on('connection', socket=>{
     console.log(`Socket conectado: ${socket.id}`);
-
+    
+    //tras mensagens antigas
     socket.emit('previousMessages', messages);
 
+    //recebe novas mensagens
     socket.on('sendMessage', data=>{
         messages.push(data);
+        //envia para todos os clientes
         socket.broadcast.emit('receivedMessage', data);
     });
 });
